2cf14153de82f707b7bb5398c1cf9973a6ed1edd
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2024-10-26 14:51:00 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.0"
+}
+
+group = "ke.hub"
+version = "0.0.1"
+
+application {
+    mainClass.set("ke.hub.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-server-csrf-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-swagger-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/ke/hub/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package ke.hub.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/demo") {
+            call.respond(
+                mapOf(
+                    "message" to "Hello World!"
+                )
+            )
+        }
+    }
+}
